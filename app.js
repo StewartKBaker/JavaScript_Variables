e7037f62dfe6218053ec7b19777ee6f8beba2d37
StewartBaker <bakrstewart16@gmail.com>
2024-08-19 09:24:54 -0500

    finished exercise

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Stewart";
+let lastName = "Baker";
+var age = 20;
 
+let language = "JavaScript"
+let createdYear = 1995;
+let isCaseSensitive = true;
 // Exercise 2
-
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStone = false;
+let selectedSize = "M";
 // Exercise 3
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
